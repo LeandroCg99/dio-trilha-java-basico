5710f71359f8b995868b88b4d8b5438756c0107f
LeandroCg99 <leandro.cgeronimo@hotmail.com>
2023-10-13 03:22:23 -0300

    finalizacao do desafio ContaBanco

---
+++ conta-banco/src/Conta.java
@@ -0,0 +1,7 @@
+public class Conta {
+
+    int numeroConta;
+    String numeroAgencia;
+    double saldo;
+    
+}
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        Cliente cliente = new Cliente();
+        Conta conta = new Conta();
+
+        System.out.println("Bem vindo ao banco DIO, digite seu nome: ");
+        cliente.nome = sc.nextLine();
+        System.out.println("\nAgora digite seu sobrenome: ");
+        cliente.sobrenome = sc.nextLine();
+
+        System.out.println("\nPor gentileza, digite o número da conta: ");
+        conta.numeroConta = sc.nextInt();
+        sc.nextLine();
+        System.out.println("\nAgora o numero da Agência: ");
+        conta.numeroAgencia = sc.nextLine();
+
+        System.out.println("\nDigite o saldo em conta: ");
+        conta.saldo = sc.nextDouble();
+
+        sc.close();
+
+        String mensagem = criarMensagemFeedback(cliente, conta);
+        System.out.println(mensagem);
+
+    }
+
+    public static String criarMensagemFeedback(Cliente cliente, Conta conta) {
+        String mensagem = "\nOlá " + cliente.nome + " " + cliente.sobrenome
+                + ", obrigado por criar uma conta em nosso banco, sua agência é " + conta.numeroConta + ", conta "
+                + conta.numeroAgencia + " e seu saldo " + conta.saldo + " já está disponível para saque";
+        return mensagem;
     }
 }
